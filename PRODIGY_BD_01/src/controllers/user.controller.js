6c1bb2682c099c12d92b8335d541295b4fcb4961
muhammad-danial-malik <1muhammad.danial31@gmail.com>
2025-06-20 19:27:02 +0500

    Refactor user controller: implement user CRUD operations, and validate user data; add user data storage using Map.

--- PRODIGY_BD_01/src/controllers/user.controller.js
+++ PRODIGY_BD_01/src/controllers/user.controller.js
@@ -1,153 +1,80 @@
-import { asyncHandler } from "../utils/asyncHandler.js";
-import User from "../models/user.model.js";
-import { ApiError } from "../utils/apiError.js";
-import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { v4 as uuid } from "uuid";
+import { isValidUser } from "../utils/validate.js";
+import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/apiResponse.js";
-import jwt from "jsonwebtoken";
+import users from "../data/users.js";
 
-const cookiesOptions = {
-  httpOnly: true,
-  secure: true,
+export const getAllUsers = (req, res) => {
+  const userList = Array.from(users.values());
+
+  if (userList.length === 0) {
+    throw new ApiError(404, "Users not found");
+  }
+
+  res
+    .status(200)
+    .json(new ApiResponse(200, userList, "Users fetched successfully"));
 };
 
-const generateAccessAndRefreshToken = async (userId) => {
-  try {
-    const user = await User.findById(userId);
+export const getUserById = (req, res) => {
+  const user = users.get(req.params.id);
 
-    const accessToken = user.generateAccessToken();
-    const refreshToken = user.generateRefreshToken();
-
-    user.refreshToken = refreshToken;
-    await user.save({ validateBeforeSave: false });
-    return { accessToken, refreshToken };
-  } catch (error) {
-    throw new ApiError(500, "Failed to generate tokens", error);
-  }
-};
-
-const registerUser = asyncHandler(async (req, res) => {
-  const { username, email, password } = req.body;
-
-  console.log("email", email);
-
-  // if (username === "" || email === "" || password === "") {
-  //   return apiError(400, "All fields are required");
-  // }
-
-  if ([username, email, password].some((field) => field?.trim() === "")) {
-    throw new ApiError(400, "All fields are required");
-  }
-
-  const existedUser = await User.findOne({ email });
-
-  if (existedUser) {
-    throw new ApiError(409, "User already exists with this email");
-  }
-
-  const user = await User.create({
-    username,
-    email,
-    password,
-  });
-
-  const createdUser = await User.findById(user._id).select(
-    "-password -refreshToken"
-  );
-
-  if (!createdUser) {
-    throw new ApiError(500, "Something went wrong while registering user");
-  }
-
-  return res
-    .status(201)
-    .json(new ApiResponse(200, createdUser, "User registered successfully"));
-});
-
-const loginUser = asyncHandler(async (req, res) => {
-  const { email, password } = req.body;
-
-  if (!email) {
-    throw new ApiError(400, "email is required");
-  }
-
-  const user = await User.findOne({ email });
-
-  if (!user) {
-    throw new ApiError(404, "User does not exist");
-  }
-
-  const isPasswordValid = await user.isPasswordCorrect(password);
-
-  if (!isPasswordValid) {
-    throw new ApiError(401, "Invalid user credentials");
-  }
-
-  const { accessToken, refreshToken } = await generateAccessAndRefreshToken(
-    user._id
-  );
-
-  const loggedInUser = await User.findById(user._Id).select(
-    "-password -refreshToken"
-  );
-
-  return res
-    .status(200)
-    .cookie("refreshToken", refreshToken, cookiesOptions)
-    .cookie("accessToken", accessToken, cookiesOptions)
-    .json(
-      new ApiResponse(
-        200,
-        { user: loggedInUser, accessToken, refreshToken },
-        "User logged in successfully"
-      )
-    );
-});
-
-const logoutUser = asyncHandler(async (req, res) => {
-  await User.findByIdAndUpdate(
-    req.user._Id,
-    {
-      $set: {
-        refreshToken: undefined,
-      },
-    },
-    {
-      new: true,
-    }
-  );
-
-  return res
-    .status(200)
-    .clearCookie("refreshToken", cookiesOptions)
-    .clearCookie("accessToken", cookiesOptions)
-    .json(new ApiResponse(200, {}, "User logged out successfully"));
-});
-
-const changeCurrentPassword = asyncHandler(async (req, res) => {
-  const { userId } = req.params;
-  const { currentPassword, newPassword } = req.body;
-
-  const user = await User.findById(userId);
   if (!user) {
     throw new ApiError(404, "User not found");
   }
 
-  const isPasswordCorrect = await user.isPasswordCorrect(currentPassword);
+  res.status(200).json(new ApiResponse(200, user, "User fetched successfully"));
+};
 
-  if (!isPasswordCorrect) {
-    throw new ApiError(401, "iinvalid current password");
+export const createUser = (req, res) => {
+  const { name, email, age } = req.body;
+
+  if (!isValidUser({ name, email, age })) {
+    throw new ApiError(400, "Invalid user data");
   }
 
-  user.password = newPassword;
-  await user.save(validateBeforeSave, false);
+  const id = uuid();
+  const newUser = { id, name, email, age };
+
+  users.set(id, newUser);
+
+  res
+    .status(201)
+    .json(new ApiResponse(201, newUser, "User created successfully"));
+};
+
+export const updateUser = (req, res) => {
+  const { id } = req.params;
+
+  if (!users.has(id)) {
+    throw new ApiError(404, "User not found");
+  }
+
+  const { name, email, age } = req.body;
+
+  if (!isValidUser({ name, email, age })) {
+    throw new ApiError(400, "Invalid user data");
+  }
+
+  const updatedUser = { id, name, email, age };
+  users.set(id, updatedUser);
 
   res
     .status(200)
-    .json(new ApiResponse(200, {}, "Password changed successfully"));
-});
+    .json(new ApiResponse(200, updatedUser, "User updated successfully"));
+};
 
-export {
-  registerUser,
-  loginUser,
-  logoutUser,
+export const deleteUser = (req, res) => {
+  const { id } = req.params;
+
+  if (!users.has(id)) {
+    throw new ApiError(404, "User not found");
+  }
+
+  const deletedUser = users.get(id);
+  users.delete(id);
+
+  res
+    .status(200)
+    .json(new ApiResponse(200, deletedUser, "User deleted successfully"));
 };
