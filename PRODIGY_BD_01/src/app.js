7ed26e35265a97ba15abdf33ce0aa8026a26b9bd
muhammad-danial-malik <1muhammad.danial31@gmail.com>
2025-06-20 18:20:50 +0500

    Refactor project structure: remove unused dependencies, middleware, and utility functions; streamline user routes; update server initialization logic.

--- PRODIGY_BD_01/src/app.js
+++ PRODIGY_BD_01/src/app.js
@@ -1,6 +1,4 @@
 import express from "express";
-import cors from "cors";
-import cookieParser from "cookie-parser";
 
 const app = express();
 
--- PRODIGY_BD_01/src/routes/user.routes.js
+++ PRODIGY_BD_01/src/routes/user.routes.js
@@ -1,20 +1,18 @@
-import { Router } from "express";
+import {Router} from 'express';
 import {
-  loginUser,
-  logoutUser,
-  registerUser,
-} from "../controllers/user.controller.js";
-import { verifyJWT } from "../middlewares/auth.middleware.js";
+  createUser,
+  getAllUsers,
+  getUserById,
+  updateUser,
+  deleteUser,
+} from '../controllers/userController.js';
 
 const router = Router();
 
-// Public routes
-router.route("/register").post(registerUser);
-router.route("/login").post(loginUser);
-
-router.use(verifyJWT);
-
-// Protected routes
-router.route("/logout").post(logoutUser);
+router.route('/').get(getAllUsers);
+router.route('/:id').get(getUserById);
+router.route('/').post(createUser);
+router.route('/:id').patch(updateUser);
+router.route('/:id').delete(deleteUser);
 
 export default router;
