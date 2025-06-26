720fe6ef0fc92a3cb2d17c96cfbed8da623ce47f
muhammad-danial-malik <1muhammad.danial31@gmail.com>
2025-06-26 21:42:03 +0500

    Setup database connection

--- PRODIGY_BD_02/src/index.js
+++ PRODIGY_BD_02/src/index.js
@@ -1,17 +1,22 @@
 import dotenv from "dotenv";
+import connectDB from "./db/connectDB.js";
 import app from "./app.js";
-import { loadUsersFromFile } from "./data/user.js";
 
 dotenv.config({
   path: "./.env",
 });
 
-app.listen(process.env.Port || 3000, async () => {
-  await loadUsersFromFile();
-  console.log(`Server is running on port ${process.env.Port || 3000}`);
-});
-
-app.on("error", (err) => {
-  console.log("Error starting server:", err);
-  process.exit(1);
-});
+connectDB()
+  .then(() => {
+    app.on("error", (err) => {
+      console.log("Error starting server:", err);
+      process.exit(1);
+    });
+    app.listen(process.env.Port || 3000, () => {
+      console.log(`Server is running on port ${process.env.Port || 3000}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Error connecting to database:", err);
+    process.exit(1);
+  });
