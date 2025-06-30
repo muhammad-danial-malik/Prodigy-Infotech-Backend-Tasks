3fcf5c35cb8b48324c2f14d50a06a6b3aaa0ada8
muhammad-danial-malik <1muhammad.danial31@gmail.com>
2025-06-30 22:12:20 +0500

    setup user model with schema definition

--- PRODIGY_BD_03/src/models/user.model.js
+++ PRODIGY_BD_03/src/models/user.model.js
@@ -1,1 +1,34 @@
+import mongoose from "mongoose";
 
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    role: {
+      type: String,
+      required: true,
+      enum: ["admin", "manager", "user"],
+      default: "user",
+    },
+  },
+  { timestamps: true }
+);
+
+const Account = mongoose.model("Account", userSchema);
+
+export default Account;
