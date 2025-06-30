25cae211c8ee9123a0b184a84307d27c7a1a3340
muhammad-danial-malik <1muhammad.danial31@gmail.com>
2025-06-30 22:42:34 +0500

    add refresh token generation, and implement JWT verification middleware

--- PRODIGY_BD_03/src/models/user.model.js
+++ PRODIGY_BD_03/src/models/user.model.js
@@ -15,16 +15,28 @@ const userSchema = new mongoose.Schema(
       required: true,
       unique: true,
       trim: true,
+      match: [
+        /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+        "Please provide a valid email address",
+      ],
     },
     password: {
       type: String,
       required: [true, "Password is required"],
+      minlength: [8, "Password must be at least 8 characters long"],
+      match: [
+        /^(?=.*[a-zA-Z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,}$/,
+        "Password must contain at least one letter and one number",
+      ],
     },
     role: {
       type: String,
       required: true,
       enum: ["admin", "manager", "user"],
       default: "user",
+    },
+    refreshToken: {
+      type: String,
     },
   },
   { timestamps: true }
@@ -39,6 +51,32 @@ userSchema.pre("save", async function (next) {
 
 userSchema.methods.isPasswordCorrect = async function (password) {
   return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
 };
 
 const Account = mongoose.model("Account", userSchema);
