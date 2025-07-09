8a73043302d8592a274c34845b1676b3152fcf17
muhammad-danial-malik <1muhammad.danial31@gmail.com>
2025-07-09 16:47:10 +0500

    finalize task fix error, and manage routing

--- PRODIGY_BD_03/src/routes/user.routes.js
+++ PRODIGY_BD_03/src/routes/user.routes.js
@@ -1,5 +1,23 @@
-import {Router} from 'express';
+import { Router } from "express";
+import {
+  registerUser,
+  loginUser,
+  logoutUser,
+  getAllUsers,
+  getProfile,
+} from "../controllers/user.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import authorizeRoles from "../middlewares/authorizeRole.middleware.js";
 
 const router = Router();
+
+// Auth routes
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+
+// Protected routes
+router.post("/logout", verifyJWT, logoutUser);
+router.get("/profile", verifyJWT, getProfile);
+router.get("/", verifyJWT, authorizeRoles("admin"), getAllUsers);
 
 export default router;
